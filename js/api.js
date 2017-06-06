2e821b0f79b7193eee3a1530f1ef1b4a9ed49131
memetibryan <memetibryan@gmail.com>
2017-06-06 17:27:39 +0300

    changed apiKey in .env

--- js/api.js
+++ js/api.js
@@ -9,7 +9,8 @@ Github.prototype.github = function(find) {
   var username = $('#input1').val();
   $('#input1').val("");
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+   console.log(response);
+   /* $("ul#output").append("<li>" + response + "</li>"); */
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
