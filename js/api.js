56afdf0dcb4a401d4e04304b0bd9c314af63c413
memetibryan <memetibryan@gmail.com>
2017-06-13 15:53:42 +0300

    changed code to display results of api request in console

--- js/api.js
+++ js/api.js
@@ -9,8 +9,7 @@ Github.prototype.github = function(find) {
   var username = $('#input1').val();
   $('#input1').val("");
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-   console.log(response);
-   /* $("ul#output").append("<li>" + response + "</li>"); */
+      console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
