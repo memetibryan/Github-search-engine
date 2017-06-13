513ab4aa244b35a9e692daf75818a535071eab30
memetibryan <memetibryan@gmail.com>
2017-06-13 17:01:48 +0300

    included comment code for storing server request locally in a variable

--- js/api.js
+++ js/api.js
@@ -10,10 +10,19 @@ Github.prototype.github = function(find) {
   $('#input1').val("");
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
       console.log(response);
+      $("ul#output").append("<ol>" + username + "-" + "Github details:-" + "check console"+ "</ol>");
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    console.log(error.responseJSON.message);    
   });
+
   return display;
+
+
 };
 
 exports.githubModule = Github;
+
+
+/*var show = $('response').val();
+      $('response').val("");
+      $("#output").html(show);*/
